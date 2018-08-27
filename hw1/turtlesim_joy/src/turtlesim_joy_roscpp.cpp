1cd40e4485f493fceb736183ea8c446bb2560c74
Ayush Shukla <ayushshukla@gatech.edu>
2018-08-27 11:36:06 +0200

    Add turtlesim_joy C++ node [HW1]

---
+++ hw1/turtlesim_joy/src/turtlesim_joy_roscpp.cpp
@@ -0,0 +1,35 @@
+#include <ros/ros.h>
+#include <sensor_msgs/Joy.h>
+#include <geometry_msgs/Twist.h>
+
+class TurtleJoy {
+    public:
+        TurtleJoy();
+    private:
+        void joyCallback(const sensor_msgs::Joy::ConstPtr& joy);
+
+        ros::NodeHandle _nh;
+        
+        ros::Publisher vel_pub;
+        ros::Subscriber joy_sub;
+
+};
+
+TurtleJoy::TurtleJoy() {
+    vel_pub = nh_.advertise<geometry_msgs::Twist>("turtle1/cmd_vel", 1);
+    joy_sub = nh_.subscribe<sensor_msgs::Joy>("joy", 10, &TurtleJoy::joyCallback, this);
+}
+
+void TurtleJoy::joyCallback(const sensor_msgs::Joy::ConstPtr& joy) {
+    geometry_msgs::Twist twist;
+    twist.angular.z = 1;
+    twist.linear.x = 1;
+    vel_pub.publish(twist);
+}
+
+int main(int argc, char** argv) {
+    ros::init(argc, argv, "turtlejoy");
+    TurtleJoy turtle_joy;
+    
+    ros::spin();
+}
