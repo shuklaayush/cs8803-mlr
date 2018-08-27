89cd48a944843c28facd5bcf33ea31af775f5b34
Ayush Shukla <ayushshukla@gatech.edu>
2018-08-27 12:24:55 +0200

    Rename Node [HW1]

--- hw1/turtlejoy/src/turtlejoy.cpp
+++ hw1/turtlejoy/src/turtlejoy.cpp
@@ -13,17 +13,26 @@ class TurtleJoy {
         ros::Publisher vel_pub;
         ros::Subscriber joy_sub;
 
+        int linear_, angular_;
+        double l_scale, a_scale;
 };
 
-TurtleJoy::TurtleJoy() {
+TurtleJoy::TurtleJoy()
+    : linear_(1),
+      angular_(2) {
     vel_pub = nh_.advertise<geometry_msgs::Twist>("turtle1/cmd_vel", 1);
     joy_sub = nh_.subscribe<sensor_msgs::Joy>("joy", 10, &TurtleJoy::joyCallback, this);
+
+    nh_.param("axis_linear", linear_, linear_);
+    nh_.param("axis_angular", angular_, angular_);
+    nh_.param("scale_linear", l_scale, 0.1);
+    nh_.param("scale_angular", a_scale, 0.1);
 }
 
 void TurtleJoy::joyCallback(const sensor_msgs::Joy::ConstPtr& joy) {
     geometry_msgs::Twist twist;
-    twist.angular.z = 1;
-    twist.linear.x = 1;
+    twist.angular.z = a_scale * joy->axes[angular_];
+    twist.linear.x = l_scale * joy->axes[linear_];
     vel_pub.publish(twist);
 }
 
