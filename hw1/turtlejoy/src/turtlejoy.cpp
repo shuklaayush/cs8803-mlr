66bb4ccec309f61073826b389def7e0c2ef25cea
Ayush Shukla <ayushshukla@gatech.edu>
2018-08-29 15:10:03 +0200

    Enable Debug logs; fix indentation [HW1]

--- hw1/turtlejoy/src/turtlejoy.cpp
+++ hw1/turtlejoy/src/turtlejoy.cpp
@@ -1,46 +1,50 @@
-#include <ros/ros.h>
-#include <sensor_msgs/Joy.h>
 #include <geometry_msgs/Twist.h>
+#include <ros/ros.h>
+#include <ros/console.h>
+#include <sensor_msgs/Joy.h>
 
 class TurtleJoy {
-    public:
-        TurtleJoy();
-    private:
-        void joyCallback(const sensor_msgs::Joy::ConstPtr& joy);
+  public:
+    TurtleJoy();
+  private:
+    void joyCallback(const sensor_msgs::Joy::ConstPtr& joy);
 
-        ros::NodeHandle nh;
-        
-        ros::Publisher vel_pub;
-        ros::Subscriber joy_sub;
+    ros::NodeHandle nh;
 
-        int linear_axis_pos = 5;
-        int linear_axis_neg = 2;
-        int angular_axis = 0;
+    ros::Publisher vel_pub;
+    ros::Subscriber joy_sub;
 
-        double l_scale = 1.0;
-        double a_scale = 1.0;
+    int l_axis_pos = 5;
+    int l_axis_neg = 2;
+    int a_axis = 0;
+
+    double l_scale = 1.0;
+    double a_scale = 1.0;
 };
 
 TurtleJoy::TurtleJoy() {
-    ROS_INFO("Starting turtlejoy with turtlejoy name %s", ros::this_node::getName().c_str());
+  ROS_INFO("Starting turtlejoy with turtlejoy name %s", ros::this_node::getName().c_str());
 
-    vel_pub = nh.advertise<geometry_msgs::Twist>("/vrep/twistCommand", 1);
-    joy_sub = nh.subscribe<sensor_msgs::Joy>("joy", 10, &TurtleJoy::joyCallback, this);
+  vel_pub = nh.advertise<geometry_msgs::Twist>("/vrep/twistCommand", 1);
+  joy_sub = nh.subscribe<sensor_msgs::Joy>("joy", 10, &TurtleJoy::joyCallback, this);
 }
 
 void TurtleJoy::joyCallback(const sensor_msgs::Joy::ConstPtr& joy) {
-    geometry_msgs::Twist twist;
-    twist.linear.x = l_scale * (joy->axes[linear_axis_neg] - joy->axes[linear_axis_pos]) / 2.0;
-    twist.angular.z = a_scale * joy->axes[angular_axis];
+  geometry_msgs::Twist twist;
+  twist.linear.x = l_scale * (joy->axes[l_axis_neg] - joy->axes[l_axis_pos]) / 2.0;
+  twist.angular.z = a_scale * joy->axes[a_axis];
 
-    ROS_DEBUG("[%s]: vel_x: %f omega_z: %f", nh.getNamespace().c_str(), twist.linear.x, twist.angular.z);
+  ROS_DEBUG("[%s]: vel_x: %f omega_z: %f", ros::this_node::getName().c_str(), twist.linear.x, twist.angular.z);
 
-    vel_pub.publish(twist);
+  vel_pub.publish(twist);
 }
 
 int main(int argc, char** argv) {
-    ros::init(argc, argv, "turtlejoy");
-    TurtleJoy turtle_joy;
-    
-    ros::spin();
+  ros::init(argc, argv, "turtlejoy");
+  if( ros::console::set_logger_level(ROSCONSOLE_DEFAULT_NAME, ros::console::levels::Debug) ) {
+    ros::console::notifyLoggerLevelsChanged();
+  }
+  TurtleJoy turtlejoy;
+
+  ros::spin();
 }
