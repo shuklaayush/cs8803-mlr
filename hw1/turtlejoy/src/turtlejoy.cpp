e1159f6e42b430417843e940db6ab0b3f750bc87
Ayush Shukla <ayushshukla@gatech.edu>
2018-09-13 15:31:57 +0200

    Modify turtlejoy to publish to right topic [HW4]

--- hw1/turtlejoy/src/turtlejoy.cpp
+++ hw1/turtlejoy/src/turtlejoy.cpp
@@ -1,16 +1,18 @@
 #include <geometry_msgs/Twist.h>
-#include <ros/ros.h>
 #include <ros/console.h>
+#include <ros/ros.h>
 #include <sensor_msgs/Joy.h>
 
 class TurtleJoy {
-  public:
+public:
     TurtleJoy();
-  private:
+
+private:
     void joyCallback(const sensor_msgs::Joy::ConstPtr& joy);
 
     ros::NodeHandle nh;
 
+    std::string cmd_vel_topic;
     ros::Publisher vel_pub;
     ros::Subscriber joy_sub;
 
@@ -23,28 +25,35 @@ class TurtleJoy {
 };
 
 TurtleJoy::TurtleJoy() {
-  ROS_INFO("Starting turtlejoy node with name %s", ros::this_node::getName().c_str());
+    ROS_INFO("Starting turtlejoy node with name %s",
+             ros::this_node::getName().c_str());
 
-  vel_pub = nh.advertise<geometry_msgs::Twist>("/vrep/drone/cmd_vel", 1);
-  joy_sub = nh.subscribe<sensor_msgs::Joy>("joy", 10, &TurtleJoy::joyCallback, this);
+    nh.param("cmd_vel", cmd_vel_topic, std::string("/vrep/twistCommand"));
+
+    vel_pub = nh.advertise<geometry_msgs::Twist>(cmd_vel_topic, 1);
+    joy_sub = nh.subscribe<sensor_msgs::Joy>("joy", 10, &TurtleJoy::joyCallback,
+                                             this);
 }
 
 void TurtleJoy::joyCallback(const sensor_msgs::Joy::ConstPtr& joy) {
-  geometry_msgs::Twist twist;
-  twist.linear.x = l_scale * (joy->axes[l_axis_neg] - joy->axes[l_axis_pos]) / 2.0;
-  // twist.angular.z = a_scale * joy->axes[a_axis];
+    geometry_msgs::Twist twist;
+    twist.linear.x =
+        l_scale * (joy->axes[l_axis_neg] - joy->axes[l_axis_pos]) / 2.0;
+    twist.angular.z = a_scale * joy->axes[a_axis];
 
-  ROS_DEBUG("[%s]: vel_x: %f omega_z: %f", ros::this_node::getName().c_str(), twist.linear.x, twist.angular.z);
+    ROS_DEBUG("[%s]: vel_x: %f omega_z: %f", ros::this_node::getName().c_str(),
+              twist.linear.x, twist.angular.z);
 
-  vel_pub.publish(twist);
+    vel_pub.publish(twist);
 }
 
 int main(int argc, char** argv) {
-  ros::init(argc, argv, "turtlejoy");
-  if( ros::console::set_logger_level(ROSCONSOLE_DEFAULT_NAME, ros::console::levels::Debug) ) {
-    ros::console::notifyLoggerLevelsChanged();
-  }
-  TurtleJoy turtlejoy;
+    ros::init(argc, argv, "turtlejoy");
+    if (ros::console::set_logger_level(ROSCONSOLE_DEFAULT_NAME,
+                                       ros::console::levels::Debug)) {
+        ros::console::notifyLoggerLevelsChanged();
+    }
+    TurtleJoy turtlejoy;
 
-  ros::spin();
+    ros::spin();
 }
