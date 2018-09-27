d62894be73ed527f3e2d13772a9c6604b1a55a14
Ayush Shukla <ayushshukla@gatech.edu>
2018-09-27 19:38:09 +0200

    Build gridmap [WIP][P1]

--- p1/floor_plane_mapping/src/floor_plane_mapping.cpp
+++ p1/floor_plane_mapping/src/floor_plane_mapping.cpp
@@ -17,42 +17,24 @@
 
 namespace {
 using std::vector;
-using Point = pcl::PointXYZ;
 using Matrix = Eigen::MatrixXf;
 using Vector = Eigen::VectorXf;
 using cv::Mat_;
 using nav_msgs::OccupancyGrid;
 using std::abs;
+using std::cout;
 
-constexpr auto DEFAULT_GRID_VAL = 100;
+constexpr auto GRID_IMAGE_DEFAULT = 50;
+constexpr auto GRID_MAX = 100;
 }  // namespace
 
-Vector find_normal(const vector<Point>& points) {
-    auto n = points.size();
-
-    Matrix A(n, 3);
-    Vector b(n);
-
-    // Linear regression: z = a*x + b*y + c
-    for (unsigned int i = 0; i < n; ++i) {
-        A(i, 0) = points[i].x;
-        A(i, 1) = points[i].y;
-        A(i, 2) = 1;
-
-        b(i, 0) = points[i].z;
-    }
-    Vector X = A.colPivHouseholderQr().solve(b);
-    return X;  // TODO: Check move semantics
-}
-
 Mat_<uint8_t> to_mat(const OccupancyGrid& grid) {
-    Mat_<uint8_t> image(grid.info.height, grid.info.width, DEFAULT_GRID_VAL);
-    // image = DEFAULT_GRID_VAL;
+    Mat_<uint8_t> image(grid.info.height, grid.info.width, GRID_IMAGE_DEFAULT);
     for (unsigned int i; i < grid.data.size(); ++i) {
         auto x = i / grid.info.width;
         auto y = i % grid.info.width;
-        if (grid.data[i] != -1) {
-            image(y, x) = 2 * (grid.data[i] + 1) - 1;
+        if (grid.data[i] >= 0) {
+            image(y, x) = (GRID_MAX - grid.data[i]) * 255 / 100;
         }
     }
     // cv_bridge::CvImage ros_image(std_msgs::Header(),"mono8",image);
@@ -71,7 +53,8 @@ protected:
 
     tf::TransformListener listener_;
 
-    std::string base_frame_;
+    std::string map_frame_;
+      std::string base_frame_;
     int width_;
     int height_;
     double resolution_;
@@ -81,6 +64,25 @@ protected:
     pcl::PointCloud<pcl::PointXYZ> lastpcl_;
 
 protected:  // ROS Callbacks
+    Vector find_normal(const vector<unsigned int>& points) {
+        auto n = points.size();
+
+        Matrix A(n, 3);
+        Vector b(n);
+
+        // Linear regression: z = a*x + b*y + c
+        for (unsigned int i = 0; i < n; ++i) {
+            auto ix = points[i];
+            A(i, 0) = lastpcl_[ix].x;
+            A(i, 1) = lastpcl_[ix].y;
+            A(i, 2) = 1;
+
+            b(i, 0) = lastpcl_[ix].z;
+        }
+        Vector X = A.colPivHouseholderQr().solve(b);
+        return X;  // TODO: Check move semantics
+    }
+
     void pcl_callback(const sensor_msgs::PointCloud2ConstPtr msg) {
         // Receive the point cloud and convert it to the right format
         pcl::PointCloud<pcl::PointXYZ> temp;
@@ -93,8 +95,7 @@ protected:  // ROS Callbacks
 
         unsigned int n = temp.size();
         // TODO: Iterate through points and store them in grid matrix
-        vector<vector<Point>> gridpoints;
-        gridpoints.reserve(width_ * height_);
+        vector<vector<unsigned int>> gridpoints(width_ * height_);
         for (unsigned int i = 0; i < n; i++) {
             float x = temp[i].x;
             float y = temp[i].y;
@@ -115,14 +116,12 @@ protected:  // ROS Callbacks
             }
             if (0 <= x && x < width_ * resolution_ && 0 <= y &&
                 y < height_ * resolution_) {
-                Point pt(x, y, z);
                 // If we reach this stage, we have an acceptable point, so
                 // let's store it
                 int x_grid = x / resolution_;
                 int y_grid = y / resolution_;
                 int ix = y_grid * width_ + x_grid;
-                std::cout << ix << std::endl;
-                gridpoints[ix].push_back(pt);
+                gridpoints[ix].push_back(i);
             }
         }
         // TODO: For each gridcell, calculate normal and modify occupancy grid
@@ -130,9 +129,15 @@ protected:  // ROS Callbacks
             auto N = find_normal(gridpoints[i]);
             auto a = N[0];
             auto b = N[1];
-            grid_.data[i] =
-                (abs(1 - (1 / (a * a + b * b + 1))) < 0.05) ? 127 : 0;
+            auto error = abs(1 - (1 / (a * a + b * b + 1)));
+            grid_.data[i] = (error < 0.5) ? 0 : GRID_MAX;
         }
+        // for (unsigned int i = 0; i < grid_.data.size(); ++i) {
+        //   cout << static_cast<int>(grid_.data[i]) << ' ';
+        //   if (i % grid_.info.width == 0) {
+        //     cout << '\n';
+        //   }
+        // }
         auto image = to_mat(grid_);
         cv_bridge::CvImage ros_image(std_msgs::Header(), "mono8", image);
         gridimage_pub_.publish(ros_image.toImageMsg());
@@ -143,6 +148,7 @@ public:
         // The parameter below described the frame in which the point cloud
         // must be projected to be estimated. You need to understand TF
         // enough to find the correct value to update in the launch file
+        nh_.param("map_frame", map_frame_, std::string("/world"));
         nh_.param("base_frame", base_frame_, std::string("/bubbleRob"));
         // This parameter defines the maximum range at which we want to
         // consider points. Experiment with the value in the launch file to
@@ -168,8 +174,8 @@ public:
         grid_.info.height = height_;
         grid_.info.resolution = resolution_;
 
-        grid_.info.origin.position.x = 0;
-        grid_.info.origin.position.y = 0;
+        grid_.info.origin.position.x = -(resolution_ * width_) / 2;
+        grid_.info.origin.position.y = (resolution_ * height_) / 2;
         grid_.info.origin.position.z = 0;
         grid_.info.origin.orientation.w = 1;
 
