5fc033907ffa111b9bde9ce21650001c7a035895
Ayush Shukla <ayushshukla@gatech.edu>
2018-10-08 13:48:11 +0200

    Make node more modular

--- p1/floor_plane_mapping/src/floor_plane_mapping.cpp
+++ p1/floor_plane_mapping/src/floor_plane_mapping.cpp
@@ -54,57 +54,43 @@ protected:
 
 protected:  // ROS Callbacks
     Mat_<uint8_t> to_mat(const OccupancyGrid& grid) {
-        Mat_<uint8_t> image(grid.info.height, grid.info.width, GRID_IMAGE_DEFAULT);
-            for (unsigned int i = 0; i < grid.data.size(); ++i) {
-                auto x = i % grid.info.width;
+        Mat_<uint8_t> image(grid.info.height, grid.info.width,
+                            GRID_IMAGE_DEFAULT);
+        for (auto i = 0U; i < grid.data.size(); ++i) {
+            auto x = i % grid.info.width;
             auto y = grid.info.height - i / grid.info.width - 1;
             if (grid.data[i] >= 0) {
                 image(y, x) = (GRID_MAX - grid.data[i]) * 255 / 100;
             }
         }
-        return image; // Check Move semantics
+        return image;  // Check Move semantics
     }
 
     Vector find_normal(const vector<unsigned int>& points) {
         auto n = points.size();
-
+        // AX = b
         Matrix A(n, 3);
         Vector b(n);
-
         // Linear regression: z = a*x + b*y + c
-        for (unsigned int i = 0; i < n; ++i) {
+        for (auto i = 0U; i < n; ++i) {
             auto ix = points[i];
             A(i, 0) = pcl_world_[ix].x;
             A(i, 1) = pcl_world_[ix].y;
             A(i, 2) = 1;
-
             b(i, 0) = pcl_world_[ix].z;
         }
         Vector X = A.colPivHouseholderQr().solve(b);
         return X;  // TODO: Check move semantics
     }
 
-    void pcl_callback(const sensor_msgs::PointCloud2ConstPtr msg) {
-        // Receive the point cloud and convert it to the right format
-        pcl::PointCloud<pcl::PointXYZ> temp;
-        pcl::fromROSMsg(*msg, temp);
-        // Make sure the point cloud is in the base-frame
-        listener_.waitForTransform(base_frame_, msg->header.frame_id,
-                                   msg->header.stamp, ros::Duration(1.0));
-        pcl_ros::transformPointCloud(base_frame_, msg->header.stamp, temp,
-                                     msg->header.frame_id, lastpcl_, listener_);
-
-        listener_.waitForTransform(map_frame_, msg->header.frame_id,
-                                   msg->header.stamp, ros::Duration(1.0));
-        pcl_ros::transformPointCloud(map_frame_, msg->header.stamp, temp,
-                                     msg->header.frame_id, pcl_world_, listener_);
-
-        unsigned int n = temp.size();
-        // TODO: Iterate through points and store them in grid matrix
-        vector<vector<unsigned int>> gridpoints(width_ * height_);
-        for (unsigned int i = 0; i < n; i++) {
-            float x = temp[i].x;
-            float y = temp[i].y;
+    vector<vector<unsigned int>> filter_points(
+        const pcl::PointCloud<pcl::PointXYZ>& pcl) {
+        // Grid to store indices of points
+        vector<vector<unsigned int>> points(width_ * height_);
+        // Filter
+        for (auto i = 0U; i < pcl.size(); ++i) {
+            float x = pcl[i].x;
+            float y = pcl[i].y;
             float d = hypot(x, y);
             // In the sensor frame, this point would be inside the camera
             if (d < 1e-2) {
@@ -123,25 +109,57 @@ protected:  // ROS Callbacks
             float y0 = grid_.info.origin.position.y;
             x = pcl_world_[i].x - x0;
             y = pcl_world_[i].y - y0;
-            if (0 <= x && x < width_ * resolution_ && 0 <= y &&
-                y < height_ * resolution_) {
-                // If we reach this stage, we have an acceptable point, so
-                // let's store it
-                int x_grid = x / resolution_;
-                int y_grid = y / resolution_;
-                int ix = y_grid * width_ + x_grid;
-                gridpoints[ix].push_back(i);
+            if (!(0 <= x && x < width_ * resolution_) ||
+                !(0 <= y && y < height_ * resolution_)) {
+                // Outside grid, ignore
+                continue;
             }
+            // Store point for update
+            int x_grid = x / resolution_;
+            int y_grid = y / resolution_;
+            int ix = y_grid * width_ + x_grid;
+            points[ix].push_back(i);
         }
-        for (unsigned int i = 0; i < gridpoints.size(); ++i) {
-            if (gridpoints[i].size() > 0) {
-                auto N = find_normal(gridpoints[i]);
+        return points;
+    }
+
+    void update_grid(const vector<vector<unsigned int>>& points) {
+        for (auto i = 0U; i < points.size(); ++i) {
+            // Only update if point was in FOV
+            if (points[i].size() > 0) {
+                auto N = find_normal(points[i]);
                 auto a = N[0];
                 auto b = N[1];
-                auto error = abs(1 - (1 / (a * a + b * b + 1)));
+                auto fit = 1.0 / (a * a + b * b + 1);
+                auto error = 1.0 - fit;
                 grid_.data[i] = (error < 0.5) ? 0 : GRID_MAX;
             }
         }
+        return;
+    }
+
+    void pcl_callback(const sensor_msgs::PointCloud2ConstPtr msg) {
+        // Receive the point cloud and convert it to the right format
+        pcl::PointCloud<pcl::PointXYZ> temp;
+        pcl::fromROSMsg(*msg, temp);
+        // Wait for transforms to become available
+        listener_.waitForTransform(base_frame_, msg->header.frame_id,
+                                   msg->header.stamp, ros::Duration(1.0));
+        listener_.waitForTransform(map_frame_, msg->header.frame_id,
+                                   msg->header.stamp, ros::Duration(1.0));
+        // Transform to new frame
+        pcl_ros::transformPointCloud(base_frame_, msg->header.stamp, temp,
+                                     msg->header.frame_id, lastpcl_, listener_);
+        pcl_ros::transformPointCloud(map_frame_, msg->header.stamp, temp,
+                                     msg->header.frame_id, pcl_world_,
+                                     listener_);
+        // Filter points
+        auto gridpoints = filter_points(temp);
+        // Update grid
+        update_grid(gridpoints);
+        // Publish grid
+        grid_pub_.publish(grid_);
+        // Generate CV::Mat of grid
         auto image = to_mat(grid_);
         cv_bridge::CvImage ros_image(std_msgs::Header(), "mono8", image);
         gridimage_pub_.publish(ros_image.toImageMsg());
