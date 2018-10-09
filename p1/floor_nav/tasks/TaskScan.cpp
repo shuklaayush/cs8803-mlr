0d7534758d845ac21bd8302cf5fd97565839bf73
Ayush Shukla <shuklaayush247@gmail.com>
2018-10-09 23:18:44 +0200

    Fix floating point comparison [P1]

--- p1/floor_nav/tasks/TaskScan.cpp
+++ p1/floor_nav/tasks/TaskScan.cpp
@@ -1,24 +1,30 @@
 #include "TaskScan.h"
-#include <math.h>
+#include <cmath>
 #include "floor_nav/TaskScanConfig.h"
 using namespace task_manager_msgs;
 using namespace task_manager_lib;
 using namespace floor_nav;
 
+namespace {
+    constexpr auto EPSILON = 0.1;
+    using std::abs;
+}
+
 TaskIndicator TaskScan::initialise() {
     ROS_INFO("Rotating with angular velocity %.2f", cfg.angular_velocity);
-    const geometry_msgs::Pose2D& tpose = env->getPose2D();
+    auto tpose = env->getPose2D();
     initial_angle = tpose.theta;
     return TaskStatus::TASK_INITIALISED;
 }
 
 TaskIndicator TaskScan::iterate() {
-    const geometry_msgs::Pose2D& tpose = env->getPose2D();
-    double d_theta = (tpose.theta >= initial_angle)
-                         ? tpose.theta - initial_angle
-                         : 2 * M_PI + tpose.theta - initial_angle;
+    auto tpose = env->getPose2D();
+    double d_theta = tpose.theta - initial_angle;
+    if (d_theta < -EPSILON / 10) {
+        d_theta += 2 * M_PI;
+    }
     // ROS_INFO("Difference: %.2f", d_theta);
-    if (d_theta > 2 * M_PI - 0.2) {
+    if (2 * M_PI - d_theta < EPSILON) {
         return TaskStatus::TASK_COMPLETED;
     }
     env->publishVelocity(0, cfg.angular_velocity);
