0836b04a2085ccfc3c005a681a32c8a99ede72c0
Ayush Shukla <ayushshukla@gatech.edu>
2018-11-08 15:29:16 +0100

    Add inference code; fix formatting [P2]

--- p2/floor_plane_deep/src/floor_plane_classifier.cpp
+++ p2/floor_plane_deep/src/floor_plane_classifier.cpp
@@ -1,150 +1,145 @@
-#include <ros/ros.h>
-#include <visualization_msgs/Marker.h>
 #include <geometry_msgs/Pose2D.h>
+#include <ros/ros.h>
 #include <tf/tf.h>
 #include <tf/transform_listener.h>
+#include <visualization_msgs/Marker.h>
 
+#include <cv_bridge/cv_bridge.h>
 #include <sensor_msgs/Image.h>
 #include <opencv2/opencv.hpp>
-#include <cv_bridge/cv_bridge.h>
 
 #include <image_transport/image_transport.h>
 #include <image_transport/transport_hints.h>
-
 
 #include <caffe/caffe.hpp>
 
 using namespace caffe;
 
-
 class CaffeAnalyzer {
-    public:
-        CaffeAnalyzer() {}
+public:
+    CaffeAnalyzer() {}
 
-        void initialize(const string& model_file,
-                const string& mean_file,
-                const string& trained_file);
+    void initialize(const string& model_file, const string& mean_file,
+                    const string& trained_file);
 
-        std::vector<float> run(const cv::Mat& img);
+    std::vector<float> run(const cv::Mat& img);
 
-    private:
-        void WrapInputLayer(std::vector<cv::Mat>* input_channels);
+private:
+    void WrapInputLayer(std::vector<cv::Mat>* input_channels);
 
-        void Preprocess(const cv::Mat& img,
-                std::vector<cv::Mat>* input_channels);
+    void Preprocess(const cv::Mat& img, std::vector<cv::Mat>* input_channels);
 
-        void SetMean(const string& mean_file);
+    void SetMean(const string& mean_file);
 
-    private:
-        shared_ptr<Net<float> > net_;
-        cv::Size input_geometry_;
-        int num_channels_;
-        cv::Mat mean_;
+private:
+    shared_ptr<Net<float> > net_;
+    cv::Size input_geometry_;
+    int num_channels_;
+    cv::Mat mean_;
 };
-
-
-
 
 class FloorPlaneClassifier {
-    protected:
-        ros::NodeHandle nh_;
-        image_transport::ImageTransport it_;
-        image_transport::Subscriber image_sub_;
-        image_transport::Publisher image_pub_;
+protected:
+    ros::NodeHandle nh_;
+    image_transport::ImageTransport it_;
+    image_transport::Subscriber image_sub_;
+    image_transport::Publisher image_pub_;
 
-        std::string model_file_;
-        std::string mean_file_;
-        std::string deploy_file_;
-        int thumb_size_;
+    std::string model_file_;
+    std::string mean_file_;
+    std::string deploy_file_;
+    int thumb_size_;
 
-        CaffeAnalyzer caffe;
+    CaffeAnalyzer caffe;
 
-        typedef enum {
-            UNUSABLE,
-            UNTRAVERSABLE,
-            TRAVERSABLE
-        } ThumbType;
+    typedef enum { UNUSABLE, UNTRAVERSABLE, TRAVERSABLE } ThumbType;
 
-        ThumbType check_thumb(const cv::Mat_<cv::Vec3b> & thumb) {
-            std::vector<float> res = caffe.run(thumb);
-            // TODO: interprete res to return a label for this image.
-            return UNUSABLE;
+    ThumbType check_thumb(const cv::Mat_<cv::Vec3b>& thumb) {
+        std::vector<float> res = caffe.run(thumb);
+        // TODO: interprete res to return a label for this image.
+        // TODO: Maybe reverse
+        if (res[0] > res[1]) {
+            return TRAVERSABLE;
+        } else {
+            return UNTRAVERSABLE;
         }
+        return UNUSABLE;
+    }
 
+protected:  // ROS Callbacks
+    void image_callback(const sensor_msgs::ImageConstPtr& img_msg) {
+        cv::Mat img(cv_bridge::toCvShare(img_msg, "bgr8")->image);
+        printf("Got 1 image\n");
 
-    protected: // ROS Callbacks
-
-
-
-        void image_callback(const sensor_msgs::ImageConstPtr & img_msg) {
-
-            cv::Mat img(cv_bridge::toCvShare(img_msg,"bgr8")->image);
-            printf("Got 1 image\n");
-
-            for (int r=0;r<img.rows;r+=thumb_size_) {
-                if (r+thumb_size_>img.rows) { continue; }
-                for (int c=0;c<img.cols;c+=thumb_size_) {
-                    // printf("r %d c %d\n",r,c);
-                    if (c+thumb_size_>img.cols) { continue; }
-                    cv::Rect roi(c,r,thumb_size_,thumb_size_);
-                    cv::Mat thumb = img(roi);
-                    ThumbType type = check_thumb(thumb);
-                    // Now for display (could be disabled to save CPU)
-                    int mark = 0;
-                    switch (type) {
-                        case UNTRAVERSABLE: 
-                            mark = 2; break;
-                        case TRAVERSABLE: 
-                            mark = 1; break;
-                        case UNUSABLE: 
-                        default:
-                            mark = 0; break;
-                    }
-                    for (int tr=0;tr<thumb.rows;tr++) {
-                        for (int tc=0;tc<thumb.cols;tc++) {
-                            thumb.at<cv::Vec3b>(tr,tc)[mark] = 255;
-                        }
-                    }
-                    // getchar();
-                }
+        for (int r = 0; r < img.rows; r += thumb_size_) {
+            if (r + thumb_size_ > img.rows) {
+                continue;
             }
-            printf("\tClassified\n");
-            
-            cv_bridge::CvImage br(img_msg->header,"bgr8",img);
-            image_pub_.publish(br.toImageMsg());
+            for (int c = 0; c < img.cols; c += thumb_size_) {
+                // printf("r %d c %d\n",r,c);
+                if (c + thumb_size_ > img.cols) {
+                    continue;
+                }
+                cv::Rect roi(c, r, thumb_size_, thumb_size_);
+                cv::Mat thumb = img(roi);
+                ThumbType type = check_thumb(thumb);
+                // Now for display (could be disabled to save CPU)
+                int mark = 0;
+                switch (type) {
+                    case UNTRAVERSABLE:
+                        mark = 2;
+                        break;
+                    case TRAVERSABLE:
+                        mark = 1;
+                        break;
+                    case UNUSABLE:
+                    default:
+                        mark = 0;
+                        break;
+                }
+                for (int tr = 0; tr < thumb.rows; tr++) {
+                    for (int tc = 0; tc < thumb.cols; tc++) {
+                        thumb.at<cv::Vec3b>(tr, tc)[mark] = 255;
+                    }
+                }
+                // getchar();
+            }
         }
+        printf("\tClassified\n");
 
-    public:
-        FloorPlaneClassifier() : nh_("~"), it_(nh_) {
-            nh_.param("thumb_size",thumb_size_,32);
-            nh_.param("model_file",model_file_,std::string("caffenet.caffemodel"));
-            nh_.param("mean_file",mean_file_,std::string("mean.binaryproto"));
-            nh_.param("deploy_file",deploy_file_,std::string("deploy.prototxt"));
-            std::string transport = "raw";
-            nh_.param("transport",transport,transport);
+        cv_bridge::CvImage br(img_msg->header, "bgr8", img);
+        image_pub_.publish(br.toImageMsg());
+    }
 
+public:
+    FloorPlaneClassifier() : nh_("~"), it_(nh_) {
+        nh_.param("thumb_size", thumb_size_, 32);
+        nh_.param("model_file", model_file_,
+                  std::string("caffenet.caffemodel"));
+        nh_.param("mean_file", mean_file_, std::string("mean.binaryproto"));
+        nh_.param("deploy_file", deploy_file_, std::string("deploy.prototxt"));
+        std::string transport = "raw";
+        nh_.param("transport", transport, transport);
 
-            caffe.initialize(deploy_file_,mean_file_,model_file_);
+        caffe.initialize(deploy_file_, mean_file_, model_file_);
 
-            image_pub_ = it_.advertise("image_label",1);
-            image_sub_ = it_.subscribe<FloorPlaneClassifier>("image",1, &FloorPlaneClassifier::image_callback,this,transport);
-            
-        }
-
+        image_pub_ = it_.advertise("image_label", 1);
+        image_sub_ = it_.subscribe<FloorPlaneClassifier>(
+            "image", 1, &FloorPlaneClassifier::image_callback, this, transport);
+    }
 };
 
-int main(int argc, char * argv[]) 
-{
-    ros::init(argc,argv,"floor_plane_classifier");
+int main(int argc, char* argv[]) {
+    ros::init(argc, argv, "floor_plane_classifier");
     FloorPlaneClassifier fp;
 
     ros::spin();
     return 0;
 }
 
-
 void CaffeAnalyzer::initialize(const string& model_file,
-        const string& mean_file, const string& trained_file) {
+                               const string& mean_file,
+                               const string& trained_file) {
 #ifdef CPU_ONLY
     Caffe::set_mode(Caffe::CPU);
 #else
@@ -156,7 +151,8 @@ void CaffeAnalyzer::initialize(const string& model_file,
     net_->CopyTrainedLayersFrom(trained_file);
 
     CHECK_EQ(net_->num_inputs(), 1) << "Network should have exactly one input.";
-    CHECK_EQ(net_->num_outputs(), 1) << "Network should have exactly one output.";
+    CHECK_EQ(net_->num_outputs(), 1)
+        << "Network should have exactly one output.";
 
     Blob<float>* input_layer = net_->input_blobs()[0];
     num_channels_ = input_layer->channels();
@@ -168,11 +164,10 @@ void CaffeAnalyzer::initialize(const string& model_file,
     SetMean(mean_file);
 }
 
-
 std::vector<float> CaffeAnalyzer::run(const cv::Mat& img) {
     Blob<float>* input_layer = net_->input_blobs()[0];
-    input_layer->Reshape(1, num_channels_,
-            input_geometry_.height, input_geometry_.width);
+    input_layer->Reshape(1, num_channels_, input_geometry_.height,
+                         input_geometry_.width);
     /* Forward dimension change to all layers. */
     net_->Reshape();
 
@@ -186,7 +181,7 @@ std::vector<float> CaffeAnalyzer::run(const cv::Mat& img) {
     Blob<float>* output_layer = net_->output_blobs()[0];
     const float* begin = output_layer->cpu_data();
     const float* end = begin + output_layer->channels();
-    return std::vector<float>(begin,end);
+    return std::vector<float>(begin, end);
 }
 
 /* Wrap the input layer of the network in separate cv::Mat objects
@@ -208,7 +203,7 @@ void CaffeAnalyzer::WrapInputLayer(std::vector<cv::Mat>* input_channels) {
 }
 
 void CaffeAnalyzer::Preprocess(const cv::Mat& img,
-        std::vector<cv::Mat>* input_channels) {
+                               std::vector<cv::Mat>* input_channels) {
     /* Convert the input image to the input image format of the network. */
     cv::Mat sample;
     if (img.channels() == 3 && num_channels_ == 1)
@@ -242,8 +237,8 @@ void CaffeAnalyzer::Preprocess(const cv::Mat& img,
      * objects in input_channels. */
     cv::split(sample_float, *input_channels);
 
-    CHECK(reinterpret_cast<float*>(input_channels->at(0).data)
-            == net_->input_blobs()[0]->cpu_data())
+    CHECK(reinterpret_cast<float*>(input_channels->at(0).data) ==
+          net_->input_blobs()[0]->cpu_data())
         << "Input channels are not wrapping the input layer of the network.";
 }
 
@@ -277,6 +272,3 @@ void CaffeAnalyzer::SetMean(const string& mean_file) {
     cv::Scalar channel_mean = cv::mean(mean);
     mean_ = cv::Mat(input_geometry_, mean.type(), channel_mean);
 }
-
-
-
